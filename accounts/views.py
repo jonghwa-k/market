44ff9a667a6c9760f01d8bd5b6946569b9d902c5
jonghwa <on9236@naver.com>
2024-09-09 23:23:17 +0900

    login

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,13 @@ class SignUpAPIView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            # 유효성 검사 통과하지 못하면 (serializer.errors,status=400) 내부적으로 실행
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
