fc40fc47e65ef56f81f76361dd1e626a434c8e08
jonghwa <on9236@naver.com>
2024-09-10 01:03:20 +0900

    products

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ from rest_framework_simplejwt.views import (
 app_name = 'accounts'
 urlpatterns = [
     path('', views.SignUpAPIView.as_view(), name='signup'),
-    path("signin/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('signin/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', views.LoginAPIView.as_view(), name='login'),
 ]
